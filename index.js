cd2d9d883ac5b0746760854e0216ae13cf5c1fa6
Prasad S Aher <prasadaher1999@gmail.com>
2021-03-01 13:56:29 +0530

    setting middlewares

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const application = express();
+const morgan = require("morgan"); 
 const portNo = 3000;
+
+//setting middlewares
+application.use(express.json())
+application.use(morgan('dev'))//for getting server request output
 
 application.listen(portNo,()=>{
     console.log("listening on port no 3000");
