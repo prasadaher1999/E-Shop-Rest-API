f9a3451d3d347b876c4ae7d078459ae17c7ad32c
Prasad S Aher <prasadaher1999@gmail.com>
2021-03-02 16:56:13 +0530

    creating order model

---
+++ models/order.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.schema;
+
+// creating schema
+
+const orderSchema = new Schema({
+    price:{type:Number,required:true},
+    product:{type:mongoose.Types.ObjectId,ref="product",required:true},
+    user:{type:mongoose.Types.ObjectId,ref:"user",required:true},
+    address:{type:String,required:true},
+    quantity:{type:Number,required:true},
+    payment_method:{type:String,required:true,default:"COD"},
+    status:{type:Boolean,default:false}
+},{timestamps: {createdAt:"created_at",updatedAt:"updated_at"}});
+
+// creating Model
+
+const Order  = mongoose.model("order",orderSchema);
+
+module.exports = { Order }
